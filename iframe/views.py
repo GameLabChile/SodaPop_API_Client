b1bd45e6ec27463aae02a7a770d3a3abb7e07553
David <dvdklein@gmail.com>
2016-01-06 13:02:09 -0300

    see scenes

--- iframe/views.py
+++ iframe/views.py
@@ -3,20 +3,25 @@ import time
 import rsa
 import base64
 import json
+import urllib2
 # Create your views here.
 def index(request):
-	return render(request, 'index.html', {})
+	teacher = "user"
+	url = "https://[server]/api/esc/?teacher="+teacher
+	res = urllib2.urlopen(url).read()
+	json_obj = json.loads(res)
+	return render(request, 'index.html', {'escs':json_obj})
 
 def form(request, esc, player):
+	url = "https://[server]/api/newalone/"
 	sign = ""
 	teacher = "user"
 	player = player
 	timereq = int(time.time())
 	esc = int(esc)
 	private_key = rsa.PrivateKey(1, 2, 3, 4, 5)
-
 	data = {'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc}
 	firma = rsa.sign( json.dumps(data, sort_keys=True) , private_key, 'SHA-1' )
 	firma64 = base64.urlsafe_b64encode(firma)
 
-	return render(request, 'form.html', {'sign':firma64, 'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc})
+	return render(request, 'form.html', {'sign':firma64, 'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc, 'url':url})
