804e47fc375601f38e7ed0362620f8656b2a23d4
David Klein <dvdklein@gmail.com>
2016-02-10 16:21:21 -0300

    ready to v0.2

--- iframe/views.py
+++ iframe/views.py
@@ -8,20 +8,25 @@ import urllib2
 def index(request):
 	teacher = "user given"
 	url = "https://[server]/api/esc/?teacher="+teacher
+	
 	res = urllib2.urlopen(url).read()
 	json_obj = json.loads(res)
 	return render(request, 'index.html', {'escs':json_obj})
 
-def form(request, esc, player):
-	url = "https://[server]/api/newalone/"
+def form(request, esc, player, classid, playerid):
+	
 	sign = ""
+	
+	url = "https://[server]/api/newalone/"
 	teacher = "user given"
+	private_key = rsa.PrivateKey() #key given
+
 	player = player
 	timereq = int(time.time())
 	esc = int(esc)
-	private_key = rsa.PrivateKey() #key given
-	data = {'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc}
+
+	data = {'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc, 'playerid': int(playerid), 'classid':int(classid)}
 	firma = rsa.sign( json.dumps(data, sort_keys=True) , private_key, 'SHA-1' )
 	firma64 = base64.urlsafe_b64encode(firma)
-
-	return render(request, 'form.html', {'sign':firma64, 'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc, 'url':url})
+	print  json.dumps(data, sort_keys=True)
+	return render(request, 'form.html', {'sign':firma64, 'teacher':teacher, 'player':player, 'time':timereq, 'esc':esc, 'url':url, 'playerid':playerid, 'classid':classid})
