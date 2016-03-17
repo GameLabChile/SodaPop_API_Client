9edc05349f0cb9dbd26be9225db7c18654d666f1
David <dvdklein@gmail.com>
2016-03-17 16:54:08 -0300

    Update urls.py

--- iframe/urls.py
+++ iframe/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	#ej: /google/
     url(r'^$', views.index, name="index"),
-    url(r'([\d]+)/([a-zA-Z0-9]+)/([\d]+)/([\d]+)/$', views.form, name="tile"),
+    url(r'([\d]+)/([a-zA-Z0-9]+)/([\d]+)/([\d]+)/([\d]+)/$', views.form, name="tile"),
 
 ]
